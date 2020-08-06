4d6588eaa0d33a4d641b8616fbc4a06e22004ecc
Ezra Tsai <ishow8166@icloud.com>
2020-08-06 12:07:18 +0800

    feat: add middleware to record request stuff in server log

--- app.js
+++ app.js
@@ -3,22 +3,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+    console.log('Request Time:', new Date(), '|', req.method, 'from', req.url)
+    next()
+})
+
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+    res.send('列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+    res.send('新增 Todo 頁面')
 })
- 
+
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+    res.send('顯示一筆 Todo')
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+    res.send('新增一筆  Todo')
 })
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+    console.log(`App running on port ${port}`)
 })
