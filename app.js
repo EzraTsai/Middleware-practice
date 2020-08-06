47e640b6e1cb8156b00294ec5bbcc01de4c43d9f
Ezra Tsai <ishow8166@icloud.com>
2020-08-06 22:37:29 +0800

    fix: calculate req adn res time lag with res.on('finish')

--- app.js
+++ app.js
@@ -5,38 +5,28 @@ const port = 3000
 
 app.use((req, res, next) => {
     const requestTime = new Date()
-    const responseTime = 1000
-    const duration = responseTime - requestTime.getUTCMilliseconds()
-    console.log('Request Time:', requestTime, '|', req.method, 'from', req.url, '| total time:', duration, 'ms')
+    res.on('finish', function(){
+        const responseTime = new Date()
+        const duration = responseTime.getUTCMilliseconds() - requestTime.getUTCMilliseconds()
+        console.log('Request Time:', requestTime, '|', req.method, 'from', req.url, '| total time:', duration, 'ms')
+    })
     next()
 })
 
 app.get('/', (req, res) => {
     res.send('列出全部 Todo')
-    const responseTime = new Date()
-    console.log('Response Time 1:', responseTime)
 })
 
 app.get('/new', (req, res) => {
     res.send('新增 Todo 頁面')
-    const responseTime = new Date()
-    console.log('Response Time 2:', responseTime)
 })
 
 app.get('/:id', (req, res) => {
     res.send('顯示一筆 Todo')
-    const responseTime = new Date()
-    console.log('Response Time 3:', responseTime)
 })
 
 app.post('/', (req, res) => {
     res.send('新增一筆  Todo')
-    const responseTime = new Date()
-    console.log('Response Time 4:', responseTime)
-})
-
-app.use((req, res, next) => {
-
 })
 
 app.listen(port, () => {
